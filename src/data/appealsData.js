35cb63145edf4d2c1fa24f410a3cc1be6d16c0b1
saahil004 <saahilghulammohammad@gmail.com>
2026-07-06 13:46:29 +0500

    appeal carousel images done

--- src/data/appealsData.js
+++ src/data/appealsData.js
@@ -1,7 +1,12 @@
+import sadaqa from '../assets/images/sadaqa.png'
+import lillah from '../assets/images/lillah.png'
+import emergency from '../assets/images/emergency.png'
+import food from '../assets/images/food.png'
+
 export const appeals = [
-  { id: 1, title: 'Sadaqah Program', desc: 'Your sadaqah purifies wealth and brings blessings to those in need.', image: '/src/assets/images/sadaqa.jpg' },
-  { id: 2, title: 'Lillah (In The Name of Allah) Program', desc: 'Give in the name of Allah and seek His pleasure. Support ongoing welfare initiatives.', image: '/src/assets/images/lillah.jpg' },
-  { id: 3, title: 'Emergency Services Program', desc: 'In times of crisis, we respond immediately to provide urgent relief and save lives.', image: '/src/assets/images/appeal-emergency.png' },
-  { id: 4, title: 'Food Bank Program', desc: 'Providing nutritious food to families in need. You can fight hunger and change lives.', image: '/src/assets/images/appeal-foodbank.png' },
-  { id: 5, title: 'Food Bank Program', desc: 'Providing nutritious food to families in need. You can fight hunger and change lives.', image: '/src/assets/images/appeal-foodbank.png' },
+  { id: 1, title: 'Sadaqah Program', desc: 'Your sadaqah purifies wealth and brings blessings to those in need.', image: sadaqa },
+  { id: 2, title: 'Lillah (In The Name of Allah) Program', desc: 'Give in the name of Allah and seek His pleasure. Support ongoing welfare initiatives.', image: lillah },
+  { id: 3, title: 'Emergency Services Program', desc: 'In times of crisis, we respond immediately to provide urgent relief and save lives.', image: emergency },
+  { id: 4, title: 'Food Bank Program', desc: 'Providing nutritious food to families in need. You can fight hunger and change lives.', image: food },
+  { id: 5, title: 'Food Bank Program', desc: 'Providing nutritious food to families in need. You can fight hunger and change lives.', image: food },
 ]
