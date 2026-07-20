c5e197588ab7d3cdcfc7aa773f7b147ef8a83405
saahil004 <saahilghulammohammad@gmail.com>
2026-07-20 15:52:33 +0500

    media, appeals (changes), get involved, donation, international partners not done

--- src/pages/Donation.jsx
+++ src/pages/Donation.jsx
@@ -1,12 +1,26 @@
 import DonationBanner from "../components/sections/DonationBanner.jsx"
 import DonationForm from '../components/sections/DonationForm.jsx'
 import TopDonors from "../components/sections/TopDonors.jsx";
+import InfiniteMarquee from "../components/ui/InfiniteMarquee.jsx";
 
 const Donation = () => {
 
+    const marqueeItems = [
+  '100% Goes to Those in Need',
+  'Trusted Since 1951',
+  'Every Donation Saves Lives',
+  'Transparent & Accountable',
+  'No Religion, No Race, No Bias',
+  'Your Zakat, Delivered Directly',
+  'Emergency Relief, Instantly',
+  'Join Millions of Donors',
+]
     return (
         <>
             <DonationBanner />
+            <div className="overflow-hidden">
+                <InfiniteMarquee items={marqueeItems}/>
+            </div>
             <DonationForm />
             <TopDonors />
         </>
